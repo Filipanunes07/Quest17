a3ff7d8df8cddfbdad3d8d566e6538da3a6dee60
anafilipa <anafilipanunes07@gmail.com>
2020-11-05 23:22:16 +0000

    new

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
+
+import React from "react";
+import axios from 'axios';
+import QuoteCard from "./component/QuoteCard"
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      cartoon:{}
+    };
+    this.getCartoon = this.getCartoon.bind(this);
+  }
+  
+
+  getCartoon() {
+    // Send the request
+    axios.get('https://simpsons-quotes-api.herokuapp.com/quotes')
+      // Extract the DATA from the received response
+      .then(response => response.data)
+      // Use this data to update the state
+      .then(data => {
+        this.setState({
+          cartoon: data[0]
+        });
+    });
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <QuoteCard cartoon={this.state.cartoon} />
+        <button type="button" onClick={this.getCartoon}>Get cartoon</button>
+      </div>
+    );
+  }
 }
 
 export default App;
